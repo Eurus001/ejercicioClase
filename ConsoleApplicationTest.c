74e543550e4c7a56fc35c6a351ebc48a7de2befa
Eurus001 <86990592+Eurus001@users.noreply.github.com>
2023-06-19 16:08:51 -0500

    Primer commit aun falta puts y flush

---
+++ ConsoleApplicationTest.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Alumno
+{
+	char nombre[20];
+	int edad;
+	char carrera[20];
+	float promedioGeneral;
+};
+
+int main() {
+
+	int nAlumnos;
+	char buffer[20];
+
+	struct Alumno* alumnos;
+
+	printf("Ingrese el numero de alumnos que desea ingresar: ");
+	fgets(buffer, sizeof(buffer), stdin);
+	nAlumnos = atoi(buffer);
+
+	alumnos = (struct Alumno*)malloc(nAlumnos * sizeof(struct Alumno));
+
+	if (alumnos == NULL) {
+		printf("Error de memoria\n");
+		return 1;
+	}
+
+	if (nAlumnos > 0)
+	{
+		for (int i = 0; i < nAlumnos; i++)
+		{
+			printf("Ingrese el nombre del alumno %d: ", i + 1);
+			fgets(alumnos[i].nombre, sizeof(alumnos[i].nombre), stdin);
+			alumnos[i].nombre[strcspn(alumnos[i].nombre, "\n")] = 0;
+
+			printf("Ingrese la edad del alumno %d: ", i + 1);
+			fgets(buffer, sizeof(buffer), stdin);
+			alumnos[i].edad = atoi(buffer);
+
+			printf("Ingrese la carrera del alumno %d: ", i + 1);
+			fgets(alumnos[i].carrera, sizeof(alumnos[i].carrera), stdin);
+			alumnos[i].carrera[strcspn(alumnos[i].carrera, "\n")] = 0;
+
+			printf("Ingrese el promedio general del alumno %d: ", i + 1);
+			fgets(buffer, sizeof(buffer), stdin);
+			alumnos[i].promedioGeneral = atof(buffer);
+		}
+
+		for (int i = 0; i < nAlumnos; i++)
+		{
+			printf("El nombre del alumno %d es %s, tiene %d de edad, estudia la carrera de %s y tiene un promedio de %.2f\n", i + 1, alumnos[i].nombre, alumnos[i].edad, alumnos[i].carrera, alumnos[i].promedioGeneral);
+		}
+	}
+
+	free(alumnos);
+
+	return 0;
+}
